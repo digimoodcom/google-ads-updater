a31b96a64217c93d9553af61bca6dd948ab7e3ed
Anthony K GROSS <anthony.k.gross@gmail.com>
2022-05-31 17:29:17 +0200

    fix issues

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 from google.ads.googleads.client import GoogleAdsClient
 
-client = GoogleAdsClient.load_from_storage("google-ads.dev.yaml")
+client = GoogleAdsClient.load_from_storage("google-ads.yaml")
